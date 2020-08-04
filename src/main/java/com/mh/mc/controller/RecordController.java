a77041f528adf49cec684dab321139748c56ee26
yiyuankafei <124633367@qq.com>
2020-08-04 15:07:53 +0800

    record operate

--- src/main/java/com/mh/mc/controller/RecordController.java
+++ src/main/java/com/mh/mc/controller/RecordController.java
@@ -1,7 +1,26 @@
 package com.mh.mc.controller;
 
+import com.mh.mc.entity.Record;
+import com.mh.mc.service.RecordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * createtime:2020
  */
+@RestController
+@RequestMapping("/record")
 public class RecordController {
+
+    @Autowired
+    RecordService recordService;
+
+    @RequestMapping("/insert")
+    public void insert(@RequestBody Record record) {
+
+        recordService.insert(record);
+
+    }
 }
